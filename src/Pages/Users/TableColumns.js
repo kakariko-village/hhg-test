361473a6b5cf9e668a660bc70d8290fb1638f103
Chau Le <>
2021-05-10 22:59:36 +0700

    Implement simple user table data

---
+++ src/Pages/Users/TableColumns.js
@@ -0,0 +1,19 @@
+const userTableColumns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'Email',
+    dataIndex: 'email',
+    key: 'email',
+  },
+  {
+    title: 'Position',
+    dataIndex: 'position',
+    key: 'position',
+  },
+];
+
+export default userTableColumns;
--- src/Pages/Users/index.tsx
+++ src/Pages/Users/index.tsx
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Table } from 'antd';
+import userTableColumns from './TableColumns';
+import './styles.scss';
+
+const PAGE_LIMIT = 5;
 
 function UsersPage() {
-  return <div>Users</div>;
+  const [userData, setUserData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  // Fetch All Users
+  useEffect(() => {
+    const fetchAllUsers = async () => {
+      setIsLoading(true);
+      try {
+        const res = await axios(`${process.env.REACT_APP_API_EP}users`);
+        setIsLoading(false);
+        if (res.status === 200) {
+          setUserData(res.data);
+          console.log('success res:', res);
+        } else {
+          console.log('error res:', res);
+        }
+      } catch (err) {
+        setIsLoading(false);
+        console.log(err);
+      }
+    };
+
+    fetchAllUsers();
+  }, []);
+  return (
+    <div className="user-table__wrapper">
+      <Table
+        columns={userTableColumns}
+        dataSource={userData}
+        loading={isLoading}
+        pagination={{
+          defaultPageSize: PAGE_LIMIT,
+          hideOnSinglePage: true,
+          pageSizeOptions: ['5', '10', '20', '50', '100'],
+          responsive: true,
+        }}
+      />
+    </div>
+  );
 }
 
 export default UsersPage;
